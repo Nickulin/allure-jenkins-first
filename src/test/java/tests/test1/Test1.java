db094ba36ddaf4a6cf369a205d037d8308a32e63
Nick Nickulin <nikolay.nm11-3@yandex.ru>
2023-01-27 13:48:48 +0300

    third commit

--- src/test/java/tests/test1/Test1.java
+++ src/test/java/tests/test1/Test1.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class Test1 {
     @Test
     public void test1() {
-        int countCard = 22;
+        int countCard = 20;
         Assert.assertEquals(countCard, 20);
         System.out.println("test1");
     }
